485c9e2c66418abf724783588b4392d044e924b2
Eric Durr <ericdurr@Erics-MacBook-Pro.local>
2016-09-07 21:42:33 -0600

    working page with some bonuses

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
 	var $gameButtons = $('.game_button');
 	var $playButton = $('#play_button');
+	var $gameChoices = $('.game_choice');
+	var $playAgainButton = $('#play_again_button');
+	var $rockPic = $('#rock_pic');
+	var $paperPic = $('#paper_pic');
+	var $scissorsPic = $('#scissors_pic');
+	var $rockPic2 = $('#rock_pic2');
+	var $paperPic2 = $('#paper_pic2');
+	var $scissorsPic2 = $('#scissors_pic2');
 	var $choiceResult = $('#choice_result');
 	var playChoice, computerChoice;
 	var $computerOptions = ['Rock', 'Paper', 'Scissors'];
@@ -11,66 +19,83 @@ $(document).ready(function(){
  	
 	$gameButtons.click(function() {
 		var $button = $(this);
-		var $buttonText = $button.text();
+		var $buttonText = $button.first().attr("alt");
+		$gameButtons.slideUp();
+		if($buttonText === 'Rock') {
+			$rockPic.slideDown();
+		} else if($buttonText === 'Paper') {
+			$paperPic.slideDown();
+		} else if($buttonText === 'Scissors') {
+			$scissorsPic.slideDown();
+		}
 		$playButton.slideDown();
 		playChoice = $buttonText;
 		$choiceResult.val(playChoice);
 	});
 	$playButton.click(function() {
 		$stats.slideDown();
+		$playAgainButton.slideDown();
 		computerChoice = ($computerOptions[Math.floor ( Math.random() * $computerOptions.length )])
 		switch(playChoice) {
 			case 'Rock':
 				if(computerChoice === 'Rock') {
-					alert("You've both chosen Rock. TIE!! Play again - ");
+					$rockPic2.slideDown();
+					console.log($('#comp_choice'));
 					$ties += 1;
 					$('#tiers').html('Ties: ' + ($ties));
 				} else if(computerChoice === 'Paper') {
-					alert("The computer's paper covers your rock. You LOSE!");
+					$paperPic2.slideDown();
+					console.log($('#comp_choice'));
 					$loses += 1;
 					$('#loser').html("Loses: " + ($loses));
 				} else {
-					alert("Your rock crushes the crap out of the computer's scissors. You Win!!");
+					$scissorsPic2.slideDown();
+					console.log($('#comp_choice'));
 					$wins += 1;
 					$('#winner').html("Wins:" + ($wins));
 				}
 				break;
 			case 'Paper':
 				if(computerChoice === 'Paper') {
-					alert("You've both chosen Paper. TIE!! Play again - ");
+					$paperPic2.slideDown();
 					$ties += 1;
 					$('#tiers').html('Ties: ' + ($ties));
 				} else if(computerChoice === 'Scissors') {
-					alert("The computer's scissors cut your paper to ribbons. You LOSE!");
+					$scissorsPic2.slideDown();
 					$loses += 1;
 					$('#loser').html("Loses: " + ($loses));
 				} else {
-					alert("Your Paper covers the computer's rock. You Win!!");
+					$rockPic2.slideDown();
 					$wins += 1;
 					$('#winner').html("Wins:" + ($wins));
 				}
 				break;
 			case 'Scissors':
 				if(computerChoice === 'Scissors') {
-					alert("You've both chosen Scissors. TIE!! Play again - ");
+					$scissorsPic2.slideDown();
 					$ties += 1;
 					$('#tiers').html('Ties:' + ($ties));
 				} else if(computerChoice === 'Rock') {
-					alert("The computer's Rock smashes your scissors. You LOSE!");
+					$rockPic2.slideDown();
 					$loses += 1;
 					$('#loser').html("Loses: " + ($loses));
 				} else {
-					alert("Your Scissors cut the computer's paper to bits. You Win!!");
+					$paperPic2.slideDown();
 					$wins += 1;
 					$('#winner').html("Wins:" + ($wins));
 				}
 				break;
 		}
+		
 		playChoice = undefined;
 		computerChoice = undefined;
 		$choiceResult.val(" ");
 		$playButton.slideUp();
-
-
+		
+	});
+	$playAgainButton.click(function() {
+		$gameChoices.slideUp();
+		$gameButtons.slideDown();
+		$playAgainButton.slideUp();
 	});
 });
